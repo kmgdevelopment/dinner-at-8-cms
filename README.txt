f68b439d552aeff77e1b2d5748d60b43d59f0569
Kristen Grote <kristen@kristengrote.com>
2023-10-18 14:53:28 -0700

    Remove Front-End Files
    - Add DDEV config
    - Enable headless mode in Craft
    - Add README file for cheatsheet reminders

---
+++ README.txt
@@ -0,0 +1,3 @@
+To run composer on ddev with craft you must add the --working-dir flag:
+
+ddev composer install --working-dir=/var/www/html/craft
--- src/assets/js/methods/helpers.js
+++
@@ -1,8 +0,0 @@
-// --------------------------------
-// Detect Media
-// returns a string for the size value 
-// of the #media hook element
-// --------------------------------
-function detectMedia() {
-  return $('#media').css('font-family');
-}
