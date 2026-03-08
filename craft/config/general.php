2ff616fe8372ae39dc3da703e4a4397ee3b0cabe
Kristen Grote <kristen@kristengrote.com>
2026-03-08 15:42:43 -0700

    Migrate Ingredients & Instructions Fields Away from Matrix/Super Table - Convert ingredients field to a native table field - Convert Instructions field to single CKEditor field - Install FeedMe for content migration - Add a temporary json twig template for FeedMe import - Temporarily disable headless mode for FeedMe imports

--- craft/config/general.php
+++ craft/config/general.php
@@ -11,7 +11,7 @@
 return [
     // Global settings
     '*' => [
-        'headlessMode' => true,
+        'headlessMode' => false,
 
         // Default Week Start Day (0 = Sunday, 1 = Monday...)
         'defaultWeekStartDay' => 1,
